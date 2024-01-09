8296c21fcf3fd628400b07acfbc12dddae7f4264
victormayowa <hardmanvicky01@gmail.com>
2024-01-09 20:07:10 +0000

    2-4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter(student => student.location === city)
+    .map(student => {
+      const gradeObject = newGrades.find(grade => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: gradeObject ? gradeObject.grade : 'N/A',
+      };
+    });
+}
