7bd05c03deafb08bd835fd3583fcca1d0f89ecdd
victormayowa <hardmanvicky01@gmail.com>
2024-01-09 20:31:36 +0000

    8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,16 @@
 export default function cleanSet(set, startString) {
-  const filteredValues = Array.from(set).filter(value => value.startsWith(startString));
-  return filteredValues.join('-').substring(startString.length);
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
