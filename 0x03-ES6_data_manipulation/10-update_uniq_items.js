2e46bf7d09832f1695164d55c03e62293d940821
victormayowa <hardmanvicky01@gmail.com>
2024-01-09 20:27:37 +0000

    rest

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,11 @@
+export default function updateUniqueItems(groceriesMap) {
+  if (!(groceriesMap instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
+  for (const [item, quantity] of groceriesMap.entries()) {
+    if (quantity === 1) {
+      groceriesMap.set(item, 100);
+    }
+  }
+}
