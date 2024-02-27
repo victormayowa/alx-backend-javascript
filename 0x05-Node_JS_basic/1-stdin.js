88fb76212d2e6119d8027d352714507704f939cd
victormayowa <hardmanvicky01@gmail.com>
2024-02-27 13:27:07 -0800

    1correce

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -4,7 +4,8 @@ console.log("Welcome to Holberton School, what is your name?");
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
+// Read the first line of input
+process.stdin.once('data', (data) => {
   const input = data.trim();
 
   if (input === 'exit') {
@@ -12,5 +13,11 @@ process.stdin.on('data', (data) => {
     process.exit();
   } else {
     console.log(`Your name is: ${input}`);
+    // Check if input is coming from a pipe (piped input)
+    if (!process.stdin.isTTY) {
+      console.log("This important software is now closing");
+      process.exit();
+    }
+	  process.exit();
   }
 });
