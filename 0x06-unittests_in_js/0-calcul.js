a70c88cbc66b354cd942785444b2c8dc37dd513a
victormayowa <hardmanvicky01@gmail.com>
2024-02-29 13:34:29 -0800

    calculi

---
+++ 0x06-unittests_in_js/0-calcul.js
@@ -0,0 +1,5 @@
+function calculateNumber(fir, sec) {
+    return Math.round(fir) + Math.round(sec);
+}
+
+module.exports = calculateNumber
---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,15 @@
+class Utils {
+  static calculateNumber (type, a, b) {
+    a = Math.round(a);
+    b = Math.round(b);
+
+    if (type === 'SUM') return a + b;
+    if (type === 'SUBTRACT') return a - b;
+    if (type === 'DIVIDE') {
+      if (b === 0) return 'Error';
+      return a / b;
+    }
+  }
+}
+
+module.exports = Utils;
