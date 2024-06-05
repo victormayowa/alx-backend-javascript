837d7ae6450bb2a51b4eb7e9928bb73c0b902b28
victormayowa <hardmanvicky01@gmail.com>
2024-06-05 01:13:04 +0100

    readmasync

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,26 +1,28 @@
-// File: 3-read_file_async.js
-
 const fs = require('fs');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
+async function countStudents(path) {
+  return new Promise((res, rej) => {
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        rej(new Error('Cannot load the database'));
       } else {
-        const lines = data.split('\n').filter(line => line.trim() !== '');
-        const numOfStudents = lines.length;
-        console.log(`Number of students: ${numOfStudents}`);
+        const rows = data.split('\n').slice(1);
+        const numOfStudents = rows.length;
+        if (rows[numOfStudents - 1] === '') {
+          rows.pop();
+        }
+        console.log(`Number of students: ${rows.length}`);
 
         const studentsByFields = {};
-        lines.forEach(line => {
-          const [firstname, lastname, age, field] = line.split(',');
+        for (const row of rows) {
+          const studentArray = row.split(',');
+          const field = studentArray[3];
           if (field in studentsByFields) {
-            studentsByFields[field].push(firstname);
+            studentsByFields[field].push(studentArray[0]);
           } else {
-            studentsByFields[field] = [firstname];
+            studentsByFields[field] = [studentArray[0]];
           }
-        });
+        }
 
         for (const field in studentsByFields) {
           if (Object.prototype.hasOwnProperty.call(studentsByFields, field)) {
@@ -28,8 +30,7 @@ function countStudents(path) {
             console.log(`Number of students in ${field}: ${list.length}. List: ${list.join(', ')}`);
           }
         }
-
-        resolve();
+        res();
       }
     });
   });
