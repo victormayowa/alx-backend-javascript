7a615126f0d2b83066fcdadace796ab1ecbe301b
victormayowa <hardmanvicky01@gmail.com>
2024-06-06 23:49:59 +0100

    api

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -3,12 +3,9 @@ const express = require('express');
 const app = express();
 const port = 7865;
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
-
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
 
-module.exports = app;
+app.listen(port, 'localhost');
+console.log(`API available on localhost port ${port}`)
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,24 @@
-const express = require('express');
+const request = require('request');
+const { expect } = require('chai');
 
-const app = express();
-const port = 7865;
-
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
+describe('payment api home', () => {
+  const URL = 'http://localhost:7865/';
+  it('test status code for GET / is 200', (done) => {
+    request.get(URL, (err, res) => {
+      if (err) done(err);
+      else {
+        expect(res.statusCode).to.be.equal(200);
+        done();
+      }
+    });
+  });
+  it('test response data for GET / is correct', (done) => {
+    request.get(URL, (err, res) => {
+      if (err) done(err);
+      else {
+        expect(res).has.property('body', 'Welcome to the payment system');
+        done();
+      }
+    });
+  });
 });
-
-app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
-});
-
-module.exports = app;
